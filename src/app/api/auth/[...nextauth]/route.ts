702df5f1fe3461b4069e10d4e500360668889710
mohamed dev <medhasnaoui833@gmail.com>
2025-02-27 09:53:57 +0000

    edit2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,8 @@
-import { NextAuthOptions } from "next-auth";
-import FacebookProvider from "next-auth/providers/facebook";
-import GoogleProvider from "next-auth/providers/google";
+// src/app/api/auth/[...nextauth]/route.ts
 
-export const authOptions: NextAuthOptions = {
-  pages: {
-    signIn: "/auth/sign-in",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID!,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          scope: "email public_profile",
-        },
-      },
-    }),
-  ],
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/authOptions"; // Adjust the import path as needed
 
-export default authOptions;
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- src/lib/nextAuth.ts
+++
@@ -1,7 +0,0 @@
-import Google from "next-auth/providers/google";
-
-export const authOptions = {
-    providers:[
-        Google
-    ]
-}
